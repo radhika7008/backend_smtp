693cec679d31e697143a83540e2152fcf71ceafd
kishor7008 <pspkbabul@gmail.com>
2024-10-29 23:10:11 +0530

    dqa

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,42 +1,36 @@
-const pdf = require("html-pdf");
-const nodeHtmlToImage = require("node-html-to-image");
+const puppeteer = require("puppeteer");
 
-// Function to convert HTML to PDF using html-pdf
-async function htmlToPdf(htmlContent, options = { format: "A4" }) {
-  return new Promise((resolve, reject) => {
-    pdf.create(htmlContent, options).toBuffer((err, buffer) => {
-      if (err) return reject(err);
-      resolve(buffer);
-    });
-  });
+async function htmlToPdf(htmlContent) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(htmlContent);
+  const pdfBuffer = await page.pdf({ format: "A4" });
+  await browser.close();
+  return pdfBuffer;
 }
 
-// Function to convert HTML to PNG using node-html-to-image
+// Function to convert HTML to PNG
 async function htmlToPng(htmlContent) {
-  try {
-    const imageBuffer = await nodeHtmlToImage({
-      html: htmlContent,
-      encoding: "buffer", // Return the image as a buffer
-    });
-    return imageBuffer;
-  } catch (error) {
-    console.error("Error generating PNG:", error);
-    throw error;
-  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(htmlContent);
+  const imageBuffer = await page.screenshot({ type: "png" });
+  await browser.close();
+  return imageBuffer;
 }
 
-// Function to generate a random string
 function generateRandomString({ length, lettersLower, lettersUpper, numbers }) {
   const lowerChars = "abcdefghijklmnopqrstuvwxyz";
   const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numberChars = "0123456789";
+
   let charPool = "";
 
   if (lettersLower) charPool += lowerChars;
   if (lettersUpper) charPool += upperChars;
   if (numbers) charPool += numberChars;
 
-  if (!charPool) {
+  if (charPool === "") {
     throw new Error("At least one character type must be selected.");
   }
 
@@ -45,10 +39,10 @@ function generateRandomString({ length, lettersLower, lettersUpper, numbers }) {
     const randomIndex = Math.floor(Math.random() * charPool.length);
     result += charPool[randomIndex];
   }
+
   return result;
 }
 
-// Function to transform an array into an object with random string values
 function transformArrayToObject(array) {
   return array.reduce((acc, item) => {
     const { tagName, ...rest } = item;
@@ -57,7 +51,6 @@ function transformArrayToObject(array) {
   }, {});
 }
 
-// Function to replace dynamic placeholders in content
 function replaceDynamicPlaceholders(content, data) {
   for (const key in data) {
     const pattern = new RegExp(`{{${key}}}`, "g");
@@ -66,7 +59,6 @@ function replaceDynamicPlaceholders(content, data) {
   return content;
 }
 
-// Export the functions
 module.exports = {
   htmlToPdf,
   htmlToPng,
